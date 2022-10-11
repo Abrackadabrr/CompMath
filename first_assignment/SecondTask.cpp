4a2c26106081dc0897cee081b4bc35e1dee1180a
Abrackadabrr <iliynea2002@ya.ru>
2022-10-12 00:12:44 +0300

    It actually works

--- first_assignment/SecondTask.cpp
+++ first_assignment/SecondTask.cpp
@@ -31,8 +31,8 @@ int main() {
     std::complex<double> i = {0., 1.};
     double freq = 100;
     double start = 0;
-    double stop = 0.3;
-    int N = 3000;
+    double stop = 3;
+    int N = 30000;
     double step = (stop - start) / N;
     double res = integrate_ossilate(g, freq, start, step, N).imag();
     std::cout.precision(20);
